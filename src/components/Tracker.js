bd75341f1e2172170fc599205e29689c0b5ce37f
joku8 <jsku2@illinois.edu>
2024-04-08 10:05:44 -0500

    move readme

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,11 +1,72 @@
-import React from "react";
-import { Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+} from "@mui/material";
 
 function Tracker() {
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    fetchRecords();
+  }, []);
+
+  const fetchRecords = () => {
+    fetch(
+      "https://script.google.com/macros/s/AKfycbw37Bi5UcBDcE7jxQBJMupvRhOa30CDlRPnljxaKbPSDi-4CvaIXPkYxREa-OI4754/exec"
+    )
+      .then((response) => response.json())
+      .then((data) => setRecords(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  };
+
+  const deleteRecord = (timestamp) => {
+    const postData = {
+      delete: true,
+      timestamp: timestamp,
+    };
+
+    fetch(
+      "https://script.google.com/macros/s/AKfycbw37Bi5UcBDcE7jxQBJMupvRhOa30CDlRPnljxaKbPSDi-4CvaIXPkYxREa-OI4754/exec",
+      {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+      }
+    )
+      .then(() => {
+        // After deletion, fetch the updated records
+        fetchRecords();
+      })
+      .catch((error) => console.error("Error deleting data:", error));
+  };
+
   return (
-    <Grid container spacing={2} direction="column">
-      Tracker
-    </Grid>
+    <List>
+      {records.map((record, index) => (
+        <ListItem key={index}>
+          <ListItemText
+            primary={`Order Total: $${record.order_total}`}
+            secondary={`Timestamp: ${record.timestamp}, Quantity: ${record.quantity}`}
+          />
+          <ListItemSecondaryAction>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => deleteRecord(record.timestamp)}
+            >
+              Delete
+            </Button>
+          </ListItemSecondaryAction>
+        </ListItem>
+      ))}
+    </List>
   );
 }
 
